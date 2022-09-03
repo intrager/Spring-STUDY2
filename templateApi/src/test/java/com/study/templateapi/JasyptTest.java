de483db6733799af1e79e9832e2d9e8ef280388a
Bruce Han <intrager@naver.com>
2022-09-03 20:29:48 +0900

    Learn about how to Setting Swagger

--- templateApi/src/test/java/com/study/templateapi/JasyptTest.java
+++ templateApi/src/test/java/com/study/templateapi/JasyptTest.java
@@ -13,7 +13,7 @@ public class JasyptTest {
         encryptor.setPassword(password);
         encryptor.setAlgorithm("PBEWithMD5AndTripleDES");
 
-        String content = "Fi8dVWNJXW8fNTEbk9n9MAU72EPZZgE3Ogq";    // 암호화 할 내용
+        String content = "";    // 암호화 할 내용
         String encryptedContent = encryptor.encrypt(content); // 암호화
         String decryptedContent = encryptor.decrypt(encryptedContent);  // 복호화
         System.out.println("encryptedContent = " + encryptedContent);
