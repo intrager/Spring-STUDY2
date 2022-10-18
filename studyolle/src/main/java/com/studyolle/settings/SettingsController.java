8bd790373ae1a3a7f2f043588152cc4392a1e077
Bruce Han <intrager@naver.com>
2022-10-18 19:48:42 +0900

    Implement Profile edit processing

--- studyolle/src/main/java/com/studyolle/settings/SettingsController.java
+++ studyolle/src/main/java/com/studyolle/settings/SettingsController.java
@@ -1,18 +1,44 @@
 package com.studyolle.settings;
 
+import com.studyolle.account.AccountService;
 import com.studyolle.account.CurrentUser;
 import com.studyolle.domain.Account;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.validation.Valid;
 
 @Controller
+@RequiredArgsConstructor
 public class SettingsController {
 
-    @GetMapping("/settings/profile")
+    private static final String SETTINGS_PROFILE_VIEW_NAME = "settings/profile";
+    private static final String SETTINGS_PROFILE_URL = "/settings/profile";
+
+    private final AccountService accountService;
+
+    @GetMapping(SETTINGS_PROFILE_URL)
     public String profileUpdateForm(@CurrentUser Account account, Model model) {
         model.addAttribute(account);
         model.addAttribute(new Profile(account)); // 채울 때 account에 있는 정보 조회해서 채워야 함
-        return "settings/profile";
+
+        return SETTINGS_PROFILE_VIEW_NAME;
     }
+
+    @PostMapping(SETTINGS_PROFILE_URL)
+    public String updateProfile(@CurrentUser Account account, @Valid Profile profile, Errors errors,
+                                Model model, RedirectAttributes attributes) {
+        if(errors.hasErrors()) {
+            model.addAttribute(account); // error에 대한 정보도 자동으로 들어감
+            return SETTINGS_PROFILE_VIEW_NAME;
+        }
+        accountService.updateProfile(account, profile);
+        attributes.addFlashAttribute("message", "프로필을 수정했습니다.");
+        return "redirect:" + SETTINGS_PROFILE_URL;
+    }// 데이터를 변경하는 작업은 서비스 쪽에 위임해서 트랜잭션 안에서 처리하겠다
 }
