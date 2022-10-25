27ea62c68ce33bc38a9aca2835ef833b554512e1
Bruce Han <intrager@naver.com>
2022-10-25 23:50:07 +0900

    PostgreSQL Setting

--- studyolle/src/main/java/com/studyolle/mail/ConsoleMailSender.java
+++ studyolle/src/main/java/com/studyolle/mail/ConsoleMailSender.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Component;
 import javax.mail.internet.MimeMessage;
 import java.io.InputStream;
 
-@Profile("local") // spring.profiles.active로 빈이 등록되도록 함
+@Profile({"local", "dev"}) // spring.profiles.active로 빈이 등록되도록 함
 @Component
 @Slf4j
 public class ConsoleMailSender implements JavaMailSender {
