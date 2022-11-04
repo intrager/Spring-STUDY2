dcf7ccce66d0d0dd223628fad5bee212d3a13f30
Bruce Han <intrager@naver.com>
2022-11-04 22:58:51 +0900

    project creation

---
+++ restfulHan/src/main/java/me/brucehan/restfulhan/RestfulHanApplication.java
@@ -0,0 +1,13 @@
+package me.brucehan.restfulhan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulHanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestfulHanApplication.class, args);
+    }
+
+}
---
+++ restfulHan/src/test/java/me/brucehan/restfulhan/RestfulHanApplicationTests.java
@@ -0,0 +1,13 @@
+package me.brucehan.restfulhan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulHanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
