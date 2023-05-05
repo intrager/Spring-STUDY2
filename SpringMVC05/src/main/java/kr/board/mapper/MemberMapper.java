53682379b3c938aa754a573527d2b9cfe2ee7918
Bruce Han <intrager@naver.com>
2023-05-06 01:57:49 +0900

    Authorization table creation
    Modify member registration UI (member class, authority class)
    
    Member registration controller, SQL modification (password encryption)
    
    Get member information by JOIN
    
    Display member's permission information (U M A)
    
    Member login (encryption password), photo registration processing
    
    Edit member information (delete permission, add new permission)

---
+++ SpringMVC05/src/main/java/kr/board/mapper/MemberMapper.java
@@ -0,0 +1,18 @@
+package kr.board.mapper; 
+
+import org.apache.ibatis.annotations.Mapper;
+
+import kr.board.entity.AuthVO;
+import kr.board.entity.Member;
+
+@Mapper // - MyBatis API
+public interface MemberMapper {
+	public int registerMember(Member member);
+	public Member validateLoginInfo(Member mvo);
+	public int updateMem(Member member);
+	public Member selectMemberInfo(String memID);
+	public String selectMemImage(String memID);
+	public void updateMemImage(Member mvo);
+	public void insertAuth(AuthVO saveAuth);
+	public void deleteAuth(String memID);
+}
